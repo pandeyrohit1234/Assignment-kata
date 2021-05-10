28c04d7d98fdeb5873365d5cb14e4ace788a35dd
Rohit kumar pandey <80757398+pandeyrohit1234@users.noreply.github.com>
2021-05-10 05:02:20 -0700

    assignment-1

---
+++ assigment Question-1/assigment Question-1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace assigment_Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            operation(4, 5);
+            Console.WriteLine(operation(4, 5));
+
+        }
+        public static Tuple<int, int> operation(int x, int y)
+        {
+            int res1;
+            int res2;
+            int num1 = x;
+            int num2 = y;
+            int lcm = 0;
+            while (x != y)
+            {
+                if (x > y)
+                {
+                    x = x - y;
+                }
+                else
+                {
+                    y = y - x;
+                }
+            }
+            lcm = (num1 * num2) / y;
+            res1 = lcm / num1;
+            res2 = lcm / num2;
+
+            return new Tuple<int, int>(res1, res2);
+        }
+    }
+}
